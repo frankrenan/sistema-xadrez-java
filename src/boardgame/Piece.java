c86b2f8d51305e567e0bdc9c3bd9416ba53abf83
Frank Renan <frank.renan.t.lima@gmail.com>
2020-12-05 11:57:17 -0400

    Começando a implementação na classe Board e Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,17 @@
+package boardgame;
+
+public class Piece { /* Classe Peça */
+
+	protected Position position;
+	private Board board;
+
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+}
